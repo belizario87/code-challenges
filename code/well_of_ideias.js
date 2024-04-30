11e2bc65554e55b5167a05b3c8202695377f9ed9
belizario87 <leonardobelizario88@gmail.com>
2024-04-29 21:53:09 -0300

    modify solution well_of_ideias.js

--- code/well_of_ideias.js
+++ code/well_of_ideias.js
@@ -1,17 +1,11 @@
 const a = ["bad", "bad", "bad"];
 const b = ["good", "bad", "bad", "bad"];
-const c = ["bad", "bad", "good", "bad", "good", "bad", "bad"];
+const c = ["bad", "bad", "good", "bad", "good", "bad", "bad", "good"];
 
 function well(x) {
   const stringFiltrada = x.filter((string) => string.includes("good"));
-  if (stringFiltrada.length === 0) {
-    return "Fail!";
-  }
-  if (stringFiltrada.length === 1) {
-    return "Publish!";
-  } else {
-    return "I smell a series!";
-  }
+  if (stringFiltrada.length === 0) return "Fail!";
+  return stringFiltrada.length <= 2 ? "Publish!" : "I smell a series!";
 }
 
 console.log(well(a));
